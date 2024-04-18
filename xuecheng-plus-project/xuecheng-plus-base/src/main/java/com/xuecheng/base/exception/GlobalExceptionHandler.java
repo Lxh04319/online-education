bf0268e39a645b86ca7430b10fb2382bc7641fce
lxh11111 <lxh3162335158@qq.com>
2024-04-19 00:41:47 +0800

    授权

--- xuecheng-plus-project/xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-project/xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -45,6 +45,9 @@ public class GlobalExceptionHandler {
 
     //记录异常
     log.error("系统异常{}",e.getMessage(),e);
+    if(e.getMessage().equals("不允许访问")){
+        return new RestErrorResponse("您没有权限操作此功能");
+    }
 
     //解析出异常信息
     RestErrorResponse restErrorResponse = new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
--- xuecheng-plus-project/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-project/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -12,6 +12,7 @@ import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,6 +34,7 @@ public class CourseBaseInfoController {
     CourseBaseInfoService courseBaseInfoService;
 
     @ApiOperation("课程分页查询接口")
+    @PreAuthorize("hasAuthority('xc_teachmanager_course_list')")//权限
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParamsDto) {
 
